7fa27749deb56a08878f37fa36158a86b191afab
Joakim Wallén <90190320+niati19@users.noreply.github.com>
2021-09-24 10:19:38 +0000

    Gjorde första saken

--- prog.py
+++ prog.py
@@ -1,1 +1,8 @@
 # Här skriver du din kod.
+
+svar = input("Vad är mätarställningen idag? ")
+g = int(svar)
+svar = input("Vad var mätarställnigen för ett år sedan? ")
+x = int(svar)
+y = g - x
+print("Antal körda mil:", y)

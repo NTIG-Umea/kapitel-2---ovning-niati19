09931f96026d3d7ab62fd807ef4073535c3cdade
Joakim Wallén <90190320+niati19@users.noreply.github.com>
2021-09-24 10:41:46 +0000

    Gjorde klart uppgift 2.1

--- prog.py
+++ prog.py
@@ -1,8 +1,13 @@
 # Här skriver du din kod.
 
 svar = input("Vad är mätarställningen idag? ")
-g = int(svar)
+z = float (svar)
 svar = input("Vad var mätarställnigen för ett år sedan? ")
-x = int(svar)
-y = g - x
+x = float (svar)
+y = z - x
 print("Antal körda mil:", y)
+
+svar = input("Antal liter bensin? ")
+c = float (svar)
+v = c / y
+print("Förbrukning per mil:", v)
